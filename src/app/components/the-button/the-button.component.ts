29e616735ce2a01574cce1f39f78f95ddea97b8c
junfang <junfang@twadmins-Mac-mini.local>
2020-09-09 18:43:56 +0800

    lint

--- src/app/components/the-button/the-button.component.ts
+++ src/app/components/the-button/the-button.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TheButtonComponent implements OnInit {
 
-  @Input() text:string
+  @Input() text: string;
 
   constructor() { }
 
